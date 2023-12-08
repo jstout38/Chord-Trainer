8b027e2bff2842989fd670508a6c0d653d6ab1e5
jstout38 <jstout38@gmail.com>
2023-12-08 18:13:13 -0500

    made chord interval

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import Image from 'next/image'
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 type chordSettingsType = {
   keys: [string, boolean][],
@@ -27,25 +27,48 @@ export default function Home() {
 
   const [ chordList, setChordList ] = useState<string[]>([]);
 
-  
-  function updateChordList() {
-    var updatedChords = [];
-    for (var i=0; i < chordState.chords.length; i++) {
-      for (var j = 0; j < chordState.keys.length; j++) {
-        if (chordState.keys[j][1]) {
-          var newChord = chordState.keys[j][0];
-          if (chordState.chords[i][1]) {
-            newChord += chordState.chords[i][0];
-            updatedChords.push(newChord)
-          }
-          
-        }        
-      }
-    }
-    setChordList(updatedChords);
-  }
+  const [ currentChord, setCurrentChord ] = useState('');
 
-  function handleClickKeys (event: React.ChangeEvent, position: number) {
+  const [ chordsStarted, setChordsStarted ] = useState(false);
+
+
+  // function updateChordList() {
+  //   var updatedChords = [];
+  //   for (var i=0; i < chordState.chords.length; i++) {
+  //     for (var j = 0; j < chordState.keys.length; j++) {
+  //       if (chordState.keys[j][1]) {
+  //         var newChord = chordState.keys[j][0];
+  //         if (chordState.chords[i][1]) {
+  //           newChord += chordState.chords[i][0];
+  //           updatedChords.push(newChord)
+  //         }
+          
+  //       }        
+  //     }
+  //   }
+  //   setChordList(updatedChords);
+  // }
+
+  useEffect(() => {
+      var updatedChords = [];
+      for (var i=0; i < chordState.chords.length; i++) {
+        for (var j = 0; j < chordState.keys.length; j++) {
+          if (chordState.keys[j][1]) {
+            var newChord = chordState.keys[j][0];
+            if (chordState.chords[i][1]) {
+              newChord += chordState.chords[i][0];
+              updatedChords.push(newChord)
+            }
+            
+          }        
+        }
+      }
+      setChordList(updatedChords);
+  }, [chordState]);
+
+
+
+  const handleClickKeys = async (event: React.ChangeEvent, position: number) => {
     const updatedChordState : [string, boolean][] = chordState.keys.map((item, index) =>
       [item[0], index === position ? !item[1] : item[1]]
     );
@@ -53,12 +76,11 @@ export default function Home() {
     setChordState({
       ...chordState,
       keys: updatedChordState,    
-    });
+      })
 
-    updateChordList();
   }
 
-  function handleClickChords (event: React.ChangeEvent, position: number) {
+  const handleClickChords = async (event: React.ChangeEvent, position: number) => {
     const updatedChordState : [string, boolean][] = chordState.chords.map((item, index) => 
       [item[0], index === position ? !item[1] : item[1]]
     );
@@ -68,14 +90,27 @@ export default function Home() {
       chords: updatedChordState,
     })
 
-    updateChordList();
+  }
+
+  const chordIntervalRef = useRef<NodeJS.Timeout | null>(null);
+
+  const startChords = async (event: React.MouseEvent<HTMLButtonElement>) => {      
+      chordIntervalRef.current = setInterval(() => {
+        var chordIndex = Math.floor(Math.random() * chordList.length);
+        setCurrentChord(chordList[chordIndex]);
+      }, 1000);    
+  }
+
+  const stopChords = async (event: React.MouseEvent<HTMLButtonElement>) => {
+    clearInterval(chordIntervalRef.current as NodeJS.Timeout);
+    chordIntervalRef.current = null;
   }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>Chords</div>
       <div>Chord trainer</div>      
-      <div>{chordList}</div>
+      {currentChord}
       <div>
         {chordState.keys.map((entry, index) => {
           var label = entry[0];
@@ -88,8 +123,7 @@ export default function Home() {
           );
           })
         }
-      </div>
-      <div>
+      
         {chordState.chords.map((entry, index) => {
           var label = entry[0];
           var isChecked = entry[1];
@@ -101,7 +135,11 @@ export default function Home() {
           );
           })
         }
-      </div>                
+
+        <button onClick={startChords}>Start Chords</button>
+        <button onClick={stopChords}>Stop Chords</button>
+      </div>
+                     
     </main>
   )
 }
